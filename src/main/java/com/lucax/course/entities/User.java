9e7be45e9af87a42c8231ab899ab6f50752989f5
Lucas dos Santos de Oliveira <lucax.oliveira96@gmail.com>
2021-08-16 17:38:56 -0300

    JPA repository, dependency injection, database seeding

--- src/main/java/com/lucax/course/entities/User.java
+++ src/main/java/com/lucax/course/entities/User.java
@@ -25,8 +25,20 @@ public class User implements Serializable{
 
     public User() {
     }
+    
 
-    public Long getId() {
+    public User(Long id, String name, String email, String phone, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
+
+
+
+	public Long getId() {
         return this.id;
     }
 
