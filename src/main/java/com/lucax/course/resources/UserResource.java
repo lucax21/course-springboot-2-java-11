f03af95cce7087d034c1596f6802d9ce609002ed
Lucas dos Santos de Oliveira <lucax.oliveira96@gmail.com>
2021-08-16 16:59:43 -0300

    user entity and resource

---
+++ src/main/java/com/lucax/course/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.lucax.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lucax.course.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User();
+		u.setName("Ana");
+		
+		return ResponseEntity.ok().body(u);
+	}
+
+}
