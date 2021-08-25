25b93b2cc0632c6f7bb20ca7230007c088c24999
Lucas Oliveira <lucax.oliveira96@gmail.com>
2021-08-25 15:30:11 -0300

    Payment, one-to-one association

--- src/main/java/com/lucax/course/config/TestConfig.java
+++ src/main/java/com/lucax/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.lucax.course.entities.Category;
 import com.lucax.course.entities.Order;
 import com.lucax.course.entities.OrderItem;
+import com.lucax.course.entities.Payment;
 import com.lucax.course.entities.Product;
 import com.lucax.course.entities.User;
 import com.lucax.course.entities.enums.OrderStatus;
@@ -81,6 +82,12 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"),o1);
+		
+		o1.setPayment(pay1);
+		
+		orderRepositoy.save(o1);
 		
 	}
 	
